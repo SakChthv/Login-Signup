088d906076d5abfde3f21a618ed90b2affc781ff
SakChthv <somsakchthv@gmail.com>
2025-07-06 13:23:43 +0700

    connect to mongodb

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import cookiePaser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
 app.use(express.json());
 app.use(cookiePaser());
 app.use(cors({ credentials: true }));
